57c6016614cedebd76e6696a8b02c73c7476306c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-06-15 11:05:25 +0300

    Stage2: Add right-left movement control for the Player

--- SunnyMood/Assets/PlayerMovment.cs
+++ SunnyMood/Assets/PlayerMovment.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
+    private float direction;
+    private float horizSpeed;
+    private Rigidbody2D rb2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        horizSpeed = 0.2f;
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        if (System.Math.Abs(direction) > 0.1f)
+        rb2D.AddForce(new Vector2(direction * horizSpeed, 0f), ForceMode2D.Impulse);
     }
 }
