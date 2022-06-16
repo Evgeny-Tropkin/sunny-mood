d504f5750161cf3e085045db3c6282586e515820
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-06-16 08:31:26 +0300

    Stage2. Change PlayerMovment.cs
    - Add flip if movement direction has been changed.

--- SunnyMood/Assets/PlayerMovment.cs
+++ SunnyMood/Assets/PlayerMovment.cs
@@ -7,12 +7,16 @@ public class PlayerMovment : MonoBehaviour
     private float direction;
     private float horizSpeed;
     private Rigidbody2D rb2D;
+    private float lastDirection;
+    private SpriteRenderer sprtRend;
 
     // Start is called before the first frame update
     void Start()
     {
         horizSpeed = 5.0f;
         rb2D = gameObject.GetComponent<Rigidbody2D>();
+        sprtRend = gameObject.GetComponent<SpriteRenderer>();
+        lastDirection = 1.0f;
     }
 
     // Update is called once per frame
@@ -23,6 +27,17 @@ public class PlayerMovment : MonoBehaviour
 
     void FixedUpdate()
     {
+        // if movement direction has been changed
+        if (System.Math.Abs(lastDirection - direction) > 1.1)
+        {
+            Flip();
+        }
         rb2D.velocity = new Vector2(direction * horizSpeed, 0.0f);
+    }
+
+    void Flip()
+    {
+        sprtRend.flipX = !sprtRend.flipX;
+        lastDirection *= -1;
     }
 }
